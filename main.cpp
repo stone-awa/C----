5223d176e325ceef7cf80ac7df7f90a8a05261ff
stone-awa <stone_awa@qq.com>
2025-06-22 16:23:14 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main(){
-    for(int i = 0; i < 10; i++) {
-        cout << "Hello, World! " << i << endl;
-    }
+int main() {
+    int x, y;
+    cin >> x >> y;
+    x = (x | x << 2) & 0x33;
+    x = (x | x << 1) & 0x55;
+    y = (y | y << 2) & 0x33;
+    y = (y | y << 1) & 0x55;
+    int z = x | y << 1;
+    cout << z << endl;
+    return 0;
 }
