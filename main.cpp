a08c2e5a851b06a3bb1249929a9ca5d42141438c
stone-awa <stone_awa@qq.com>
2025-06-30 21:24:08 +0800

    mm

--- main.cpp
+++ main.cpp
@@ -2,17 +2,5 @@
 using namespace std;
 
 int main(){
-    int n, s[105][105], c = 1;
-    cin >> n;
-    int t = (n + 1) / 2, q = (n + 1) / 2;
-    for (int i = 1; i <= n*n;i++){
-        s[t][q] = i;
-        c = (c + 2);
-        if ()
-    }
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            printf("%+8d",s[i][j])
-    }
-    }
+    printf("%s","Hello,World!");
 }
